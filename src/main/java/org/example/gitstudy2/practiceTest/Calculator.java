28e801fa02b6034f32bd5e561a801c02a8e977b9
duddnddnfl <duddnddnfl@naver.com>
2024-09-08 19:36:14 +0900

    test

--- src/main/java/org/example/gitstudy2/practiceTest/Calculator.java
+++ src/main/java/org/example/gitstudy2/practiceTest/Calculator.java
@@ -1,2 +1,10 @@
-package org.example.gitstudy2.practiceTest;public class Calculator {
+package org.example.gitstudy2.practiceTest;
+public class Calculator {
+    public int  sum(int a, int b){
+        if(a < 0 || b < 0){
+            throw new RuntimeException();
+        }
+        return a + b;
+    }
+
 }
--- src/test/java/org/example/gitstudy2/practiceTest/CalculatorTest.java
+++ src/test/java/org/example/gitstudy2/practiceTest/CalculatorTest.java
@@ -1,4 +1,22 @@
+package org.example.gitstudy2.practiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
+    @Test
+    void sum(){
+        //given
+
+
+        //when
+
+
+        //then
+    }
   
 }
